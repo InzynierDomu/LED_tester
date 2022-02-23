53d4b2c050b088818e0ff7d34574a04d021c039e
Inżynier Domu <inzynier.domu@gmail.com>
2022-02-23 16:25:08 +0100

    split test file to header and source

--- test/native/Color_Controller_test.cpp
+++ test/native/Color_Controller_test.cpp
@@ -1,27 +1,7 @@
-#include "../test/native/mocks/Hal_mock.h"
-#include "Color_tester/Color_tester_controller.h"
-#include "Color_tester/Color_tester_model.h"
-#include "Color_tester/Color_tester_view.h"
-
-#include "gtest/gtest.h"
+#include "../test/native/Color_Controller_test.h"
 
 namespace Color_tester
 {
-
-class Color_Controller_test : public testing::Test
-{
-  protected:
-  Color_Controller_test()
-  : hal()
-  , view(hal, model)
-  , uut(hal, model, view)
-  {}
-
-  Hal_mock hal;
-  Color_tester_model model;
-  Color_tester_view view;
-  Color_tester_controller uut;
-};
 
 TEST_F(Color_Controller_test, dummy)
 {
---
+++ test/native/Color_Controller_test.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "../test/native/mocks/Hal_mock.h"
+#include "Color_tester/Color_tester_controller.h"
+#include "Color_tester/Color_tester_model.h"
+#include "Color_tester/Color_tester_view.h"
+
+#include "gtest/gtest.h"
+
+namespace Color_tester
+{
+
+class Color_Controller_test : public testing::Test
+{
+  protected:
+  Color_Controller_test()
+  : hal()
+  , view(hal, model)
+  , uut(hal, model, view)
+  {}
+
+  Hal_mock hal;
+  Color_tester_model model;
+  Color_tester_view view;
+  Color_tester_controller uut;
+};
+
+}
