8fe0e4c4d7f4f0fb34be4b6c349392e7123beb30
Inżynier Domu <inzynier.domu@gmail.com>
2022-04-27 00:16:34 +0200

    add tests

--- test/native/PWM_chart_controller_test.cpp
+++ test/native/PWM_chart_controller_test.cpp
@@ -9,4 +9,32 @@ PWM_chart_controller_test::PWM_chart_controller_test()
 , uut(hal_mock, model, &view_mock)
 {}
 
+TEST_F(PWM_chart_controller_test, active)
+{
+  EXPECT_CALL(view_mock, print_screen());
+  EXPECT_CALL(hal_mock, set_keyboard_callback(_, _));
+  uut.active();
+}
+
+TEST_F(PWM_chart_controller_test, next_math_fun)
+{
+  EXPECT_CALL(view_mock, clear_cursor());
+  EXPECT_CALL(view_mock, update_cursor());
+  uut.keyboar_reaction(Cursor_move::down);
+  ASSERT_EQ(model.position, 1);
+}
+
+TEST_F(PWM_chart_controller_test, move_pwm_cursor)
+{
+  EXPECT_CALL(view_mock, clear_pwm_cursor()).Times(2);
+  EXPECT_CALL(view_mock, update_pwm_cursor()).Times(2);
+  EXPECT_CALL(hal_mock, set_PWM_output(_)).Times(2);
+  uut.keyboar_reaction(Cursor_move::right);
+  ASSERT_EQ(model.pwm_cursor_position, 1);
+  ASSERT_EQ(model.duty, 1);
+  uut.keyboar_reaction(Cursor_move::right);
+  ASSERT_EQ(model.pwm_cursor_position, 2);
+  ASSERT_EQ(model.duty, 2);
+}
+
 } // namespace PWM_chart
