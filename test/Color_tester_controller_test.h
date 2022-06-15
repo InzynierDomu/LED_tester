a4ced86a8655c442ebf0ce4eddcbb4df36eb6e5d
Inżynier Domu <inzynier.domu@gmail.com>
2022-06-15 15:36:37 +0200

    try with native

--- test/Color_tester_controller_test.h
+++ test/Color_tester_controller_test.h
@@ -16,7 +16,7 @@ struct Color_tester_controller_test : public ::testing::Test
 {
   protected:
   Color_tester_controller_test();
-  
+
   Hal_mock hal_mock;
   Color_tester_model model;
   Color_tester_view_mock view_mock;
